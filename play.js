2034fb98b01ab2cb2903d1fd2800acdaf108aae0
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-31 14:46:33 -0500

    refactored with Gary

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
-const net = require('net');
 
 
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-  return conn;
-};
-
-
-// refactored with Konstantin
-console.log("Connecting....");
+const {connect} = require('./client');
 const conn = connect();
-conn.on('data', (data) => {
-  console.log(data);
-});
+
+
 
 
