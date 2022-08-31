288d2f8c61b6401200a321f8700f1a81476664a8
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-31 14:46:20 -0500

    refactored with Gary

--- client.js
+++ client.js
@@ -1,12 +1,29 @@
+const net = require('net');
+const connect = function() {
 
-const connect = function () {
+ 
+  console.log("Connecting....");
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
 
+
+  conn.on('connect', () => {
+    console.log('Connected to server')
+    conn.write('Name: JWG')
+  });
+
+
+  
+
   conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
   return conn;
 };
 
-module.exports = {connect}
+module.exports = { connect };
