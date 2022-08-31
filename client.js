896c2ee5e533366023af495a8814994b24a7b913
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-31 17:28:34 -0500

    refactored

--- client.js
+++ client.js
@@ -8,14 +8,14 @@ const connect = function() {
     port: 50541
   });
 
-
+ //refactored with Gary to write name UPON connection to server
   conn.on('connect', () => {
-    console.log('Connected to server')
-    conn.write('Name: JWG')
+    console.log('Connected to server');
+    conn.write('Name: JWG');
+    // conn.write('Move: down');
+    // setTimeout(() => {conn.write('Move: left')}, 150)
+    
   });
-
-
-  
 
   conn.setEncoding('utf8');
 
